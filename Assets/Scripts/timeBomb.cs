72def5c2b7e8e70e2190688e56e3fca90bd4f75c
Littlepim <66339152+Littlepim@users.noreply.github.com>
2021-05-03 15:59:05 +1000

    work

--- Assets/Scripts/timeBomb.cs
+++ Assets/Scripts/timeBomb.cs
@@ -4,5 +4,27 @@ using UnityEngine;
 
 public class timeBomb : MonoBehaviour
 {
-    
+    [SerializeField]
+    float explosionRange;
+    [SerializeField]
+    float timedBomb;
+    [SerializeField]
+    float explostionPower;
+
+    void Start()
+    {
+        Invoke("Explode", timedBomb);
+    }
+
+    void Explode()
+    {
+        Collider[] hitObjects = Physics.OverlapSphere(transform.position, explosionRange);
+
+        foreach (Collider hitItem in hitObjects)
+        {
+            hitItem.attachedRigidbody?.AddExplosionForce(explostionPower, transform.position, explosionRange, 3.0f);
+        }
+        Destroy(gameObject);
+
+    }
 }
